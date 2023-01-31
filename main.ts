3505c4ce74e15bad8f4293a34b0d3cd2cfd0a876
ohiobabie709 <122560397+ohiobabie709@users.noreply.github.com>
2023-01-31 13:36:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showString("A")
+    PA += 1
+    rounds += 1
+    update_scoreboard()
+    basic.clearScreen()
 })
 function update_scoreboard () {
     OLED.clear()
@@ -12,22 +16,33 @@ function update_scoreboard () {
     OLED.writeStringNewLine("Rounds:" + rounds)
 }
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showString("T")
+    rounds += 1
+    Ties += 1
+    update_scoreboard()
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showString("B")
+    Pb += 1
+    rounds += 1
+    update_scoreboard()
+    basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    reset()
 })
-let Pb = 0
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("shall we play a game on this nice day?")
+    PA = 0
+    rounds = 0
+    Ties = 0
+    Pb = 0
+    basic.pause(2000)
+}
 let Ties = 0
+let Pb = 0
 let rounds = 0
 let PA = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("shall we play a game on this nice day?")
-PA = 0
-rounds = 0
-Ties = 0
-Pb = 0
-basic.pause(2000)
+reset()
